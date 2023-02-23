7f8960cea5eee43bd5024a3361a7648ff11b67cc
Ivan Filippov <iffilippov@yandex.ru>
2023-02-23 15:04:06 +0300

    Todolist application created. Technical specification functions are implemented. checkAllTaskCompleted function in main.js requires minor changes.

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# ToDoList
-Test problem for SolarLab Angular course
+## Todolist
+
+### Тестовое задание для курсов Angular SolarLab
+
+### Техническое задание
+
+Разработать приложение для создания списка TODO.
+
+```
+Приложение должно содержать поле ввода и список дел.
+```
+
+```
+Строка списка должна состоять из чекбокса, названия дела и кнопки удалить.
+```
+
+```
+Для сохранения списка необходимо использовать LocalStorage.
+```
+
+### Технологии:
+
+HTML5
+
+CSS
+
+JavaScript
+
+### Запуск проекта в VSCode:
+
+Клонировать репозиторий
+
+```
+git clone https://github.com/iffilippov/ToDoList.git
+```
+
+Установить Live Server от Ritwick Dey
+
+```
+Открыть панель расширений (Ctrl + Shift + X)
+```
+
+```
+Найти и установить Live Server
+```
+
+Запустить Live Server
+
+```
+Открыть меню выбора действий нажатием правой клавиши мышки на файле index.html
+```
+
+```
+Выбрать "Open with Live Server"
+```
+
+### Автор: [Иван Филиппов](https://www.linkedin.com/in/iffilippov/) <img src="https://github.com/blackcater/blackcater/raw/main/images/Hi.gif" width="50" height="50"/>
---
+++ js/main.js
@@ -0,0 +1,130 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
+const emptyList = document.querySelector('#emptyList');
+
+let tasks = [];
+
+getFromLocalStorage()
+checkEmptyList();
+
+form.addEventListener('submit', addTask);
+tasksList.addEventListener('click', deleteTask);
+tasksList.addEventListener('click', completedTask);
+tasksList.addEventListener('click', checkAllTaskCompleted);
+
+function addTask(event) {
+	event.preventDefault();
+
+	const taskText = taskInput.value;
+	const newTask = {
+		id: Date.now(),
+		text: taskText,
+		completed: false,
+	};
+
+	tasks.push(newTask);
+	saveToLocalStorage();
+	renderTask(newTask);
+	taskInput.value = '';
+	taskInput.focus();
+	checkEmptyList();
+}
+
+function deleteTask(event) {
+	if (event.target.dataset.action !== 'delete') return;
+
+	const parentNode = event.target.closest('.list-group-item');
+	const id = Number(parentNode.id);
+
+	tasks = tasks.filter((task) => task.id !== id);
+	saveToLocalStorage();
+	parentNode.remove();
+	checkEmptyList();
+}
+
+function completedTask(event) {
+	if (event.target.dataset.action !== 'completed') return;
+
+	const parentNode = event.target.closest('.list-group-item');
+	const id = Number(parentNode.id);
+	const task = tasks.find((task) => task.id === id);
+
+	task.completed = !task.completed;
+	saveToLocalStorage();
+
+	const taskTitle = parentNode.querySelector('.task-title');
+
+	taskTitle.classList.toggle('task-title--completed');
+}
+
+function checkEmptyList() {
+	if (tasks.length === 0) {
+		const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+					<img src="./static/img/task_list.svg" alt="Empty" width="48" class="mt-3">
+					<div class="empty-list__title">Любимый список задач - пустой список задач.</div>
+				</li>`;
+		tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+	}
+
+	if (tasks.length > 0) {
+		const emptyListElement = document.querySelector('#emptyList');
+		emptyListElement ? emptyListElement.remove() : null;
+	}
+}
+
+function saveToLocalStorage() {
+	localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function getFromLocalStorage() {
+	if (localStorage.getItem('tasks')) {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+		tasks.forEach((task) => renderTask(task));
+	}
+}
+
+function renderTask(task) {
+	const cssClass = task.completed ? 'task-title task-title--completed' : 'task-title';
+	const taskHTML = `
+                <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+					<span class="${cssClass}">${task.text}</span>
+					<div class="task-item__buttons">
+						<button type="button" data-action="completed" class="btn-action">
+							<img src="./static/img/tick.svg" alt="Completed" width="18" height="18">
+						</button>
+						<button type="button" data-action="delete" class="btn-action">
+							<img src="./static/img/cross.svg" alt="Completed" width="18" height="18">
+						</button>
+					</div>
+				</li>`;
+
+	tasksList.insertAdjacentHTML('beforeend', taskHTML);	
+}
+
+function checkAllTaskCompleted() {
+	const taskCompletedHTML = document.getElementById('completedTask');
+
+	let everythingCompletedCounter = 0;
+	
+	tasks.forEach(function(task) {
+		if (task.completed == true) {
+ 			everythingCompletedCounter = everythingCompletedCounter + 1;
+		}
+	})
+	
+	if (taskCompletedHTML == null && everythingCompletedCounter === tasks.length && everythingCompletedCounter !== 0) {
+		const allTasksCompletedHTML = `<li id="completedTask" class="list-group-item empty-list">
+						<img src="./static/img/completed.svg" alt="Empty" width="48" class="mt-3">
+						<div class="empty-list__title">Все задачи выполнены!</div>
+					</li>`;
+		tasksList.insertAdjacentHTML('beforeend', allTasksCompletedHTML);
+	}
+	else {
+		if (taskCompletedHTML != null) {
+			taskCompletedHTML.remove();
+		}
+	}
+}
